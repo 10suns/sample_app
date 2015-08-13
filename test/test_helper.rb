88fc54427a4cde5a5de67e579c3bdb033882bee1
10suns <tranthanhan0903@gmail.com>
2015-08-13 16:19:47 +0700

    Finish log in/log out.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,28 +11,26 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   # Including the Application helper in tests.
   include ApplicationHelper
-  # Return true if a test user is logged in
+  # Returns true if a test user is logged in
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Log in a test user
-  def log_in_as(user, options ={} )
-    password = options[:password] || 'passwrord'
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
     if integration_test?
-      post login_path, session: {
-        email:       user.email,
-        password:    password,
-        remember_me: remember_me
-      }
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
     else
       session[:user_id] = user.id
     end
   end
 
   private
-    # Return true inside an integration test
+    # Returns true inside an integration test.
     def integration_test?
       defined?(post_via_redirect)
     end
